474c44b9b3bfb4d8cef34d67eecea38e984488e5
seho <1018715564@qq.com>
2022-01-18 21:59:57 +0800

    🌈 style: 替换基础信息

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '2f42f08b83184ea6932ad958c3de2042',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Vue3 源码之路',
+  domain: 'seho-doc.notion.site',
+  author: 'Seho',
 
   // open graph metadata (optional)
   description: 'Example site description',
